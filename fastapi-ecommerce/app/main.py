9e3e95dc0664c47a54aa531b3720fa47c67af674
hamzasand <mhamzasaeed762@gmail.com>
2026-06-19 20:45:01 +0500

    add product functionality for get

--- fastapi-ecommerce/app/main.py
+++ fastapi-ecommerce/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from service.products import get_all_products
 
 app = FastAPI()
 
@@ -6,7 +7,6 @@ app = FastAPI()
 def root():
     return {"message": "Welcome to fastapi."}
 
-@app.get("/products/{id}")
-def get_products(id: int):
-    products = ['brush', "laptop","mouse","monitor"]
-    return products[id]
+@app.get("/products")
+def get_products():
+    return get_all_products
