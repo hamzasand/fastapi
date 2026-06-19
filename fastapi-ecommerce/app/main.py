d7694122c9da3cf72c377eec80e28709f852e4dc
hamzasand <mhamzasaeed762@gmail.com>
2026-06-19 19:26:28 +0500

    add product root in main.py

--- fastapi-ecommerce/app/main.py
+++ fastapi-ecommerce/app/main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Welcome to fastapi."}
+
+@app.get("/products/{id}")
+def get_products(id: int):
+    products = ['brush', "laptop","mouse","monitor"]
+    return products[id]
