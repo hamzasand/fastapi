7d7837d938c88bad8141db10531a8da7d5e6c359
hamzasand <mhamzasaeed762@gmail.com>
2026-06-19 21:27:30 +0500

    get with query

--- fastapi-ecommerce/app/main.py
+++ fastapi-ecommerce/app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, HTTPException
 from service.products import get_all_products
+
 
 app = FastAPI()
 
@@ -7,6 +8,31 @@ app = FastAPI()
 def root():
     return {"message": "Welcome to fastapi."}
 
+# @app.get("/products")
+# def get_products():
+#     return get_all_products
+
 @app.get("/products")
-def get_products():
-    return get_all_products
+def list_products(
+    name: str = Query(
+        default=None,
+        min_length =1,
+        max_length =20,
+        description="search product by name (case insenstive)"
+    )
+
+):
+    products = get_all_products()
+
+    if name:
+        needle = name.strip().lower()
+        products = [p for p in products if needle in p.get("name", "").lower()]
+
+        if not products:
+            raise HTTPException(
+                status_code = 404, details=f"no product found matching name={name}"
+            )
+        
+        total = len(products)
+
+    return ("total":total,"item":products)
