205fa453e34497dcf29b5b3ae7a4fe142663177a
hamzasand <mhamzasaeed762@gmail.com>
2026-06-20 13:24:06 +0500

    add querry filters

--- fastapi-ecommerce/app/main.py
+++ fastapi-ecommerce/app/main.py
@@ -19,6 +19,10 @@ def list_products(
         min_length =1,
         max_length =20,
         description="search product by name (case insenstive)"
+    ),
+    sort_by_price: bool =Query(default=False, description="Sort products by price"),
+    order: str=Query(
+        default="asc", description="Sort order when sort_by_price=true (asc,desc)"
     )
 
 ):
@@ -28,11 +32,15 @@ def list_products(
         needle = name.strip().lower()
         products = [p for p in products if needle in p.get("name", "").lower()]
 
-        if not products:
-            raise HTTPException(
-                status_code = 404, details=f"no product found matching name={name}"
-            )
+    if not products:
+        raise HTTPException(
+            status_code = 404, detail=f"no product found matching name={name}"
+        )
+    
+    if sort_by_price:
+        reverse = order == "desc"
+        products = sorted(products, key=lambda p: p.get("price",0), reverse=reverse)
         
-        total = len(products)
+    total = len(products)
 
-    return ("total":total,"item":products)
+    return {"total":total,"item":products}
