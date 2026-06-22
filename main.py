257c6a5dbf4975f9edbc21fa0ea4734f32e9359d
hamzasand <mhamzasaeed762@gmail.com>
2026-06-22 06:33:31 +0500

    path parameter,dynamic path add

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ app= FastAPI()
 @app.get("/")
 def home():
     return {"message": "Hello from fasapi"}
+
+# Dynamic route
+
+@app.get("/user/{user_id}")
+def get_user(user_id: str):
+    return {"user_id": user_id}
