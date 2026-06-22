fe964c61585df3612c36bc0d874bae9f937c0916
hamzasand <mhamzasaeed762@gmail.com>
2026-06-22 09:52:39 +0500

    basic pydantic model schema add

--- main_pydantic_model.py
+++ main_pydantic_model.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+app = FastAPI()
+
 class User(BaseModel):
     name: str
     age: int
     email: str
 
-@app.post("create_user")
+@app.post("/create_user")
 def creat_user(user: User):
     return {
         "message": "user created sucesfully",
