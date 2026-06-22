3520932e22b5a20e5d171ad8076b80646446722e
hamzasand <mhamzasaeed762@gmail.com>
2026-06-22 06:43:59 +0500

    query para and default value

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ from fastapi import FastAPI
 
 app= FastAPI()
 
+# home route
 @app.get("/")
 def home():
     return {"message": "Hello from fasapi"}
 
 # Dynamic route
-
 @app.get("/user/{user_id}")
-def get_user(user_id: str):
+def get_user(user_id: int):
     return {"user_id": user_id}
+
+# querry parameters + Default value
+@app.get("/items")
+def get_users(name: str=None, price: int=10):
+    return {
+        "name":name,
+        "price": price
+    }
